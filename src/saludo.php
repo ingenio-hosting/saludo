c18d96228d517a06be5d9a4a354ab9e66109aee0
ingeniohosting <ingenio.tarragona@gmail.com>
2024-10-18 15:51:10 +0200

    LIBERIA SALUDO ESTABLE

--- src/saludo.php
+++ src/saludo.php
@@ -5,13 +5,13 @@ Class Saludo{
     private $msn;
     private $usuario;
 
-    public function __construct($usuario){
-        $this->setSaludo();
+    public function __construct($usuario){        
         $this->usuario = $usuario;
+        $this->setSaludo();
     }
 
     private function setSaludo(){
-        $this->msn = "Bienvenido, Sr/Sra.".$this->usuario;
+        $this->msn = "Bienvenido, Sr/Sra. ".$this->usuario;
     }
 
     public function getSaludo(){
